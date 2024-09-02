a20e4057ee8d8f1266b9ea44e4e0294d738fa96c
ForestoAY <forestoay@gmail.com>
2024-09-02 19:20:01 +0700

    feat: add migrations

---
+++ server/migrations/20240902115704-create-card.js
@@ -0,0 +1,55 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Cards', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      type: {
+        type: Sequelize.STRING
+      },
+      frameType: {
+        type: Sequelize.STRING
+      },
+      desc: {
+        type: Sequelize.TEXT
+      },
+      atk: {
+        type: Sequelize.INTEGER
+      },
+      def: {
+        type: Sequelize.INTEGER
+      },
+      level: {
+        type: Sequelize.INTEGER
+      },
+      race: {
+        type: Sequelize.STRING
+      },
+      attribute: {
+        type: Sequelize.STRING
+      },
+      image_url: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Cards');
+  }
+};
