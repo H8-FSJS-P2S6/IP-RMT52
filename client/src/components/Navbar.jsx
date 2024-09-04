fd242552a8a89da9dbbb87cbae8050ca260d2259
ForestoAY <forestoay@gmail.com>
2024-09-04 20:51:18 +0700

    refactor: add link in image logo

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -12,13 +12,15 @@ export default function Navbar() {
     <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-800 h-16">
       <div className="max-w-screen-xl flex items-center justify-between mx-auto p-4 h-full">
         <div className="flex items-center space-x-3 rtl:space-x-reverse">
-          <img
-            src="/images/Yu-Gi-Oh_logo.png"
-            className="h-8"
-            alt="Yugioh Logo"
-          />
+          <Link to={"/cards"}>
+            <img
+              src="/images/Yu-Gi-Oh_logo.png"
+              className="h-8"
+              alt="Yugioh Logo"
+            />
+          </Link>
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-            Yugioh Analyzer
+            Yugioh Card List
           </span>
         </div>
 
