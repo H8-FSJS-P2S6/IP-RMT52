268bd30cdc1d5956a825dacdb47a8473a828cd61
ForestoAY <forestoay@gmail.com>
2024-09-04 11:42:13 +0700

    style: add style to navbar and rootlayout for more scalable

--- client/src/layouts/RootLayout.jsx
+++ client/src/layouts/RootLayout.jsx
@@ -1,11 +1,15 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
 
 export default function RootLayout() {
   return (
     <>
       <Navbar />
-      <Outlet />
+      {localStorage.getItem("access_token") && <Sidebar />}
+      <div className="container w-full mt-16 sm:pl-64">
+        <Outlet />
+      </div>
     </>
   );
 }
