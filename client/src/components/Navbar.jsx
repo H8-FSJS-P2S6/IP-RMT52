d68fca2a2ace5ec7db7613a4bb94e20999024d37
ForestoAY <forestoay@gmail.com>
2024-09-04 16:50:03 +0700

    style: make logout button dark red

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -27,7 +27,7 @@ export default function Navbar() {
           {localStorage.getItem("access_token") ? (
             <button
               type="button"
-              className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
+              className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-red-700 dark:hover:bg-red-700 dark:focus:ring-red-800"
               onClick={handleLogout}
             >
               Logout
