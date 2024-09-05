c86638c9155951007641ab30d284cfe6b8e53bfe
ForestoAY <forestoay@gmail.com>
2024-09-05 16:57:20 +0700

    refactor: add validation stock cant be negative

--- server/models/favorite.js
+++ server/models/favorite.js
@@ -16,7 +16,22 @@ module.exports = (sequelize, DataTypes) => {
     {
       userId: DataTypes.INTEGER,
       cardId: DataTypes.INTEGER,
-      stock: DataTypes.INTEGER,
+      stock: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+          notEmpty: {
+            msg: "Stock is required",
+          },
+          notNull: {
+            msg: "Stock is required",
+          },
+          min: {
+            args: [0],
+            msg: "Stock cannot be negative",
+          },
+        },
+      },
     },
     {
       sequelize,
