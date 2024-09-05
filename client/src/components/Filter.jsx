9b51075eb582d3b5342fe41f9a07448e423f06f1
ForestoAY <forestoay@gmail.com>
2024-09-05 20:53:57 +0700

    style: make the search, sort, filter become responsive

--- client/src/components/Filter.jsx
+++ client/src/components/Filter.jsx
@@ -8,15 +8,14 @@ import {
 
 export default function Filter() {
   const { archetypes } = useSelector((state) => state.archetypes);
-  const { search, archetype, sort } = useSelector(
-    (state) => state.filter
-  );
+  const { search, archetype, sort } = useSelector((state) => state.filter);
 
   const dispatch = useDispatch();
+
   return (
-    <>
+    <div>
       {/* Search, Filter, and Sort */}
-      <div className="mb-4 flex flex-col space-y-4 sm:flex-row sm:space-y-0 sm:space-x-4">
+      <div className="mb-4 flex flex-col gap-4 sm:flex-col sm:w-full lg:flex-row lg:space-x-4">
         {/* Search */}
         <input
           type="text"
@@ -26,7 +25,7 @@ export default function Filter() {
             dispatch(setSearch(e.target.value));
             dispatch(setPage(1));
           }}
-          className="p-2 border border-gray-300 rounded-md"
+          className="p-2 border border-gray-300 rounded-md w-full sm:w-full lg:w-auto"
         />
 
         {/* Filter */}
@@ -36,7 +35,7 @@ export default function Filter() {
             dispatch(setArchetype(e.target.value));
             dispatch(setPage(1));
           }}
-          className="p-2 border border-gray-300 rounded-md"
+          className="p-2 border border-gray-300 rounded-md w-full sm:w-full lg:w-auto"
         >
           <option value="">All Archetypes</option>
           {archetypes.map((type) => (
@@ -50,12 +49,12 @@ export default function Filter() {
         <select
           value={sort}
           onChange={(e) => dispatch(setSort(e.target.value))}
-          className="p-2 border border-gray-300 rounded-md"
+          className="p-2 border border-gray-300 rounded-md w-full sm:w-full lg:w-auto"
         >
           <option value="asc">Sort by Level (Ascending)</option>
           <option value="desc">Sort by Level (Descending)</option>
         </select>
       </div>
-    </>
+    </div>
   );
 }
