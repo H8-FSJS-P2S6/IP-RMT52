1eb4186b8c76dc7c17948f0ce0dd329795ae3819
ForestoAY <forestoay@gmail.com>
2024-09-06 09:15:24 +0700

    style: remove login button

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -26,7 +26,7 @@ export default function Navbar() {
 
         <div className="flex items-center space-x-3 md:space-x-0 rtl:space-x-reverse">
           <DarkModeToggle />
-          {localStorage.getItem("access_token") ? (
+          {localStorage.getItem("access_token") && (
             <button
               type="button"
               className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-red-700 dark:hover:bg-red-700 dark:focus:ring-red-800"
@@ -34,15 +34,6 @@ export default function Navbar() {
             >
               Logout
             </button>
-          ) : (
-            <Link to={"/login"}>
-              <button
-                type="button"
-                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-              >
-                Login
-              </button>
-            </Link>
           )}
         </div>
       </div>
